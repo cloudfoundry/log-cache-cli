ca4609b4111ee18cf19cdbf33b91428b9cb778c9
Andrew Poydence <apoydence@pivotal.io>
2018-03-09 06:49:12 -0700

    Fixes query tests

--- internal/command/query_test.go
+++ internal/command/query_test.go
@@ -2,6 +2,7 @@ package command_test
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"time"
 
@@ -35,56 +36,58 @@ var _ = Describe("Query", func() {
 	})
 
 	It("executes and returns result from query", func() {
+		now := time.Now()
 		f := func(sourceID string, start, end time.Time) []string {
 			switch sourceID {
 			case "source-x":
 				return []string{
-					`{"timestamp":"300100000002","sourceId":"source-x","counter":{"name":"x","total":"100"},"tags":{"deployment":"other"}}`,
-					`{"timestamp":"300100000003","sourceId":"source-x","counter":{"name":"x","total":"1"},"tags":{"deployment":"cf","__name__":"other","source_id":"other"}}`,
-					`{"timestamp":"300100000004","sourceId":"source-x","counter":{"name":"x","total":"100"},"tags":{"deployment":"other"}}`,
-					`{"timestamp":"301000000000","sourceId":"source-x","counter":{"name":"other","total":"2"}}`,
-					`{"timestamp":"400000101179","sourceId":"source-x","counter":{"name":"x","total":"3"},"tags":{"deployment":"cf"}}`,
+					fmt.Sprintf(`{"timestamp":"%d","sourceId":"source-x","counter":{"name":"x","total":"100"},"tags":{"deployment":"other"}}`, now.Add(-10*time.Second).UnixNano()),
+					fmt.Sprintf(`{"timestamp":"%d","sourceId":"source-x","counter":{"name":"x","total":"1"},"tags":{"deployment":"cf","__name__":"other","source_id":"other"}}`, now.Add(-10*time.Second).UnixNano()),
+					fmt.Sprintf(`{"timestamp":"%d","sourceId":"source-x","counter":{"name":"x","total":"100"},"tags":{"deployment":"cf"}}`, now.Add(-9*time.Second).UnixNano()),
+					fmt.Sprintf(`{"timestamp":"%d","sourceId":"source-x","counter":{"name":"other","total":"2"}}`, now.Add(-8*time.Second).UnixNano()),
+					fmt.Sprintf(`{"timestamp":"%d","sourceId":"source-x","counter":{"name":"x","total":"3"},"tags":{"deployment":"cf"}}`, now.UnixNano()),
 				}
 			case "source-y":
 				return []string{
-					`{"timestamp":"300080080103","sourceId":"source-y","counter":{"name":"y","total":"10"}}`,
-					`{"timestamp":"301000000000","sourceId":"source-y","gauge":{"metrics":{"other":{"value":7}}}}`,
-					`{"timestamp":"400000000000","sourceId":"source-y","gauge":{"metrics":{"y":{"value":12}}}}`,
+					fmt.Sprintf(`{"timestamp":"%d","sourceId":"source-y","counter":{"name":"y","total":"10"},"tags":{"deployment":"cf"}}`, now.Add(-10*time.Second).UnixNano()),
+					fmt.Sprintf(`{"timestamp":"%d","sourceId":"source-y","gauge":{"metrics":{"other":{"value":7}}}}`, now.Add(-9*time.Second).UnixNano()),
+					fmt.Sprintf(`{"timestamp":"%d","sourceId":"source-y","gauge":{"metrics":{"y":{"value":12}}}}`, now.Add(-8*time.Second).UnixNano()),
 				}
 			default:
 				panic("unexpected source-id")
 			}
 		}
-		args := []string{`max(x{source_id="source-x",deployment="cf"}) + max(y{source_id="source-y"})`, "--start-time", "300000000000", "--end-time", "400000000000"}
+		args := []string{`max_over_time(x{source_id="source-x",deployment="cf"}[1h])+max_over_time(y{source_id="source-y",deployment="cf"}[1h])`}
 		command.Query(context.Background(), cliConn, f, args, httpClient, logger, writer)
 
-		Expect(strings.Join(writer.lines(), " ")).To(Equal("{} => 11 @[300000] 15 @[400000]"))
+		Expect(strings.Join(writer.lines(), " ")).To(ContainSubstring("=> 110 @["))
 	})
 
 	It("executes the tail command with specified start and end time for each metric", func() {
 		var (
 			sourceIDs []string
-			starts    []time.Time
-			ends      []time.Time
+			starts    []int64
+			ends      []int64
 		)
 
 		tailer := func(sourceID string, start, end time.Time) []string {
 			sourceIDs = append(sourceIDs, sourceID)
-			starts = append(starts, start)
-			ends = append(ends, end)
+			starts = append(starts, start.UnixNano())
+			ends = append(ends, end.UnixNano())
 			return nil
 		}
-		args := []string{`x{source_id="source-x"} + y{source_id="source-y"}`, "--start-time", "300000000000", "--end-time", "600000000000"}
+		args := []string{`max_over_time(x{source_id="source-x",deployment="cf"}[1h])+max_over_time(y{source_id="source-y",deployment="cf"}[1h])`}
 		command.Query(context.Background(), cliConn, tailer, args, httpClient, logger, writer)
 
 		Expect(sourceIDs).To(ConsistOf("source-x", "source-y"))
+		now := time.Now().UnixNano()
 		Expect(starts).To(ConsistOf(
-			time.Unix(0, 300000000000),
-			time.Unix(0, 300000000000),
+			BeNumerically("~", now-int64(time.Hour), 10*time.Minute),
+			BeNumerically("~", now-int64(time.Hour), 10*time.Minute),
 		))
 		Expect(ends).To(ConsistOf(
-			time.Unix(0, 600000000000),
-			time.Unix(0, 600000000000),
+			BeNumerically("~", now, 10*time.Minute),
+			BeNumerically("~", now, 10*time.Minute),
 		))
 	})
 
@@ -109,7 +112,7 @@ var _ = Describe("Query", func() {
 
 	It("fatally logs if the metric does not have a source_id label", func() {
 		f := func(sourceID string, start, end time.Time) []string { return nil }
-		args := []string{`x + y{source_id="source-y"}`, "--start-time", "300000000000", "--end-time", "600000000000"}
+		args := []string{`max_over_time(x[1h])+max_over_time(y{source_id="source-y",deployment="cf"}[1h])`}
 		Expect(func() {
 			command.Query(context.Background(), cliConn, f, args, httpClient, logger, writer)
 		}).To(Panic())
@@ -135,7 +138,7 @@ var _ = Describe("Query", func() {
 			}
 		}
 
-		args := []string{`max(x{source_id="source-x"}) + max(y{source_id="source-y"})`, "--start-time", "300000000000", "--end-time", "400000000000"}
+		args := []string{`max_over_time(x{source_id="source-x",deployment="cf"}[1h])+max_over_time(y{source_id="source-y",deployment="cf"}[1h])`}
 		Expect(func() {
 			command.Query(context.Background(), cliConn, f, args, httpClient, logger, writer)
 		}).To(Panic())
