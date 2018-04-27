3d1101bf441a3fea9059ae6c83cf5b68e5f09ba6
Todd Persen <tpersen@pivotal.io>
2018-04-27 12:22:39 -0600

    Move shared test structures into suite
    [#156827581]
    
    Signed-off-by: Peter Stone <pstone@pivotal.io>

--- internal/command/tail_test.go
+++ internal/command/tail_test.go
@@ -4,17 +4,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"net/url"
 	"os"
 	"strconv"
-	"strings"
-	"sync"
 	"time"
 
-	"code.cloudfoundry.org/cli/plugin"
-	"code.cloudfoundry.org/cli/plugin/models"
 	"code.cloudfoundry.org/log-cache-cli/internal/command"
 
 	. "github.com/onsi/ginkgo"
@@ -911,160 +905,6 @@ var _ = Describe("LogCache", func() {
 	})
 
 })
-
-type stubLogger struct {
-	fatalfMessage  string
-	printfMessages []string
-}
-
-func (l *stubLogger) Fatalf(format string, args ...interface{}) {
-	l.fatalfMessage = fmt.Sprintf(format, args...)
-	panic(l.fatalfMessage)
-}
-
-func (l *stubLogger) Printf(format string, args ...interface{}) {
-	l.printfMessages = append(l.printfMessages, fmt.Sprintf(format, args...))
-}
-
-type stubWriter struct {
-	bytes []byte
-}
-
-// stubWriter implements io.Writer
-func (w *stubWriter) Write(p []byte) (int, error) {
-	w.bytes = append(w.bytes, p...)
-	return len(p), nil
-}
-
-func (w *stubWriter) lines() []string {
-	return strings.Split(strings.TrimSpace(string(w.bytes)), "\n")
-}
-
-type stubHTTPClient struct {
-	mu            sync.Mutex
-	responseCount int
-	responseBody  []string
-	responseCode  int
-	responseErr   error
-
-	requestURLs    []string
-	requestHeaders []http.Header
-}
-
-func newStubHTTPClient() *stubHTTPClient {
-	return &stubHTTPClient{
-		responseCode: http.StatusOK,
-		responseBody: []string{},
-	}
-}
-
-func (s *stubHTTPClient) Do(r *http.Request) (*http.Response, error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	s.requestURLs = append(s.requestURLs, r.URL.String())
-	s.requestHeaders = append(s.requestHeaders, r.Header)
-
-	var body string
-	if s.responseCount < len(s.responseBody) {
-		body = s.responseBody[s.responseCount]
-	}
-
-	resp := &http.Response{
-		StatusCode: s.responseCode,
-		Body: ioutil.NopCloser(
-			strings.NewReader(body),
-		),
-	}
-
-	s.responseCount++
-
-	return resp, s.responseErr
-}
-
-func (s *stubHTTPClient) requestCount() int {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	return len(s.requestURLs)
-}
-
-type stubCliConnection struct {
-	plugin.CliConnection
-
-	apiEndpointErr error
-
-	hasAPIEndpoint    bool
-	hasAPIEndpointErr error
-
-	cliCommandArgs   [][]string
-	cliCommandResult [][]string
-	cliCommandErr    []error
-
-	usernameResp string
-	usernameErr  error
-	orgName      string
-	orgErr       error
-	spaceName    string
-	spaceErr     error
-
-	accessTokenCount int
-	accessToken      string
-	accessTokenErr   error
-}
-
-func newStubCliConnection() *stubCliConnection {
-	return &stubCliConnection{
-		hasAPIEndpoint: true,
-	}
-}
-
-func (s *stubCliConnection) ApiEndpoint() (string, error) {
-	return "https://api.some-system.com", s.apiEndpointErr
-}
-
-func (s *stubCliConnection) HasAPIEndpoint() (bool, error) {
-	return s.hasAPIEndpoint, s.hasAPIEndpointErr
-}
-
-func (s *stubCliConnection) CliCommandWithoutTerminalOutput(args ...string) ([]string, error) {
-	s.cliCommandArgs = append(s.cliCommandArgs, args)
-	commandIndex := len(s.cliCommandArgs) - 1
-
-	if len(s.cliCommandResult) <= commandIndex {
-		return nil, errors.New("INVALID TEST SETUP")
-	}
-	var err error
-	if len(s.cliCommandErr) > commandIndex {
-		err = s.cliCommandErr[commandIndex]
-	}
-	return s.cliCommandResult[commandIndex], err
-}
-
-func (s *stubCliConnection) Username() (string, error) {
-	return s.usernameResp, s.usernameErr
-}
-
-func (s *stubCliConnection) GetCurrentOrg() (plugin_models.Organization, error) {
-	return plugin_models.Organization{
-		plugin_models.OrganizationFields{
-			Name: s.orgName,
-		},
-	}, s.orgErr
-}
-
-func (s *stubCliConnection) GetCurrentSpace() (plugin_models.Space, error) {
-	return plugin_models.Space{
-		plugin_models.SpaceFields{
-			Name: s.spaceName,
-		},
-	}, s.spaceErr
-}
-
-func (s *stubCliConnection) AccessToken() (string, error) {
-	s.accessTokenCount++
-	return s.accessToken, s.accessTokenErr
-}
 
 func responseBody(startTime time.Time) string {
 	// NOTE: These are in descending order.
