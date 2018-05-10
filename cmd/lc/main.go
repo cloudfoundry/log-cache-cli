e6d58cbe2182db5e4adddfb898706a9dea06e5c0
Mark Alston <malston@pivotal.io>
2018-05-10 14:27:52 -0600

    Add version ldflag to lc main
    [#157369209]
    
    Signed-off-by: Jason Keene <jkeene@pivotal.io>

--- cmd/lc/main.go
+++ cmd/lc/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"os"
 
 	"code.cloudfoundry.org/log-cache-cli/pkg/command"
 )
+
+// version is set via ldflags at compile time. It should be JSON encoded
+// VersionType.
+var version string
+
+type VersionType struct {
+	Major int
+	Minor int
+	Build int
+}
+
+func init() {
+	// TODO: do something with version information
+	var v VersionType
+	_ = json.Unmarshal([]byte(version), &v)
+}
 
 func main() {
 	if command.Execute(command.WithOutput(os.Stdout)) != nil {
