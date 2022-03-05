1ce6a0235f4748f4b3cd8cf45879ba0e26176da3
Carson Long <lcarson@vmware.com>
2022-03-04 18:31:27 -0800

    Moved logger interface to its own file

--- internal/command/command.go
+++ internal/command/command.go
@@ -11,12 +11,6 @@ import (
 // Command is the interface to implement plugin commands
 type Command func(ctx context.Context, cli plugin.CliConnection, args []string, c HTTPClient, log Logger, w io.Writer)
 
-// Logger is used for outputting log-cache results and errors
-type Logger interface {
-	Fatalf(format string, args ...interface{})
-	Printf(format string, args ...interface{})
-}
-
 // HTTPClient is the client used for HTTP requests
 type HTTPClient interface {
 	Do(req *http.Request) (*http.Response, error)
