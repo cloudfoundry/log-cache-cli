9f7892eb10336fb300a4615134187e54e937a782
Carson Long <lcarson@vmware.com>
2022-04-19 09:21:21 -0700

    Update: github.com/onsi/ginkgo/v2 (#76)
    * Bump github.com/onsi/ginkgo to v2.
    * Created a tools.go file to bring in packages as tools that we don't
      want to include in builds.
    * Added ginkgo CLI as a tool.
    * Use vendored ginkgo CLI to run tests in actions

--- internal/command/query_test.go
+++ internal/command/query_test.go
@@ -6,8 +6,7 @@ import (
 	"net/url"
 
 	"code.cloudfoundry.org/log-cache-cli/v4/internal/command"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
