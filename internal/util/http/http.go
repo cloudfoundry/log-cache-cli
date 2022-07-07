073cbeb7fa8a85f9bf5e02c040329dc241a2e2c8
Carson Long <lcarson@vmware.com>
2022-07-07 10:42:51 -0600

    Add Go Doc comments to the http package

--- internal/util/http/http.go
+++ internal/util/http/http.go
@@ -1,18 +1,23 @@
+// Package http provides HTTP client implementations.
 package http
 
 import "net/http"
 
-// Client is the client used for HTTP requests
+// A Client is implemented by the standard library's http.Client and
+// TokenClient.
 type Client interface {
 	Do(req *http.Request) (*http.Response, error)
 }
 
-// TokenClient is an interface representing an authenticated HTTP client
+// A TokenClient wraps an HTTP client to automatically set Authorization headers
+// in requests using the provided function to generate tokens.
 type TokenClient struct {
 	c         Client
 	tokenFunc func() string
 }
 
+// NewTokenClient returns a TokenClient given a client and token-generating
+// funtion.
 func NewTokenClient(c Client, tf func() string) *TokenClient {
 	return &TokenClient{
 		c:         c,
@@ -20,6 +25,9 @@ func NewTokenClient(c Client, tf func() string) *TokenClient {
 	}
 }
 
+// Do makes an HTTP request using the underlying client. If the token function
+// returns a non-empty string then it will be set as the Authorization header of
+// the request.
 func (c *TokenClient) Do(req *http.Request) (*http.Response, error) {
 	accessToken := c.tokenFunc()
 	if len(accessToken) > 0 {
