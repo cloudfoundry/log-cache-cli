8a7fc34882fd1367a3ae85f4e31fa80bec9cd4a8
Carson Long <lcarson@vmware.com>
2023-03-01 21:20:08 -0800

    Upgrade go-loggregator to v9

--- internal/command/formatter.go
+++ internal/command/formatter.go
@@ -9,7 +9,7 @@ import (
 	"text/template"
 	"time"
 
-	"code.cloudfoundry.org/go-loggregator/v8/rpc/loggregator_v2"
+	"code.cloudfoundry.org/go-loggregator/v9/rpc/loggregator_v2"
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
